f24255a15df8bc1f9ac3b8d017ccea9c7063c234
seantshi <seantshi@hawaii.edu>
2016-10-09 14:15:19 -1000

    Task 2: Tasklist setup

---
+++ app/tasklist.js
@@ -0,0 +1,15 @@
+
+Tasks = new Mongo.Collection('tasks');
+
+if (Meteor.isClient) {
+  Template.tasks.helpers({
+    tasks: function() {
+      return Tasks.find({}, {sort: {createdAt: -1}});
+    }
+  });
+}
+
+if (Meteor.isServer) {
+
+}
+
